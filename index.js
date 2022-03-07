9b508998ec6641ec9a750d31efee54365d27e9a2
AlejandroMolinosEligio <alejandromoeli@gmail.com>
2022-03-07 10:39:24 +0100

    Actualización para /cool

---
+++ index.js
@@ -0,0 +1,17 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use("/", express.static('public'));
+
+app.get("/cool",(req,res)=>{
+    console.log("Requested / route");
+    res.send("<html><body>"+cool()+"</body></html>");
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor listo ${port}`)
+});
