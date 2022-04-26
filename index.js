35cb30ee6455dea560083c3df2a6b10952f16981
“gabriellb99” <gabrilopezbellido@gmail.com>
2022-04-26 14:44:42 +0200

    home v2

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 8081;
 
 const population_levels_API = require("./src/back/population-levels/v2/API population-levels.js");
 const population_levels_API_v1 = require("./src/back/population-levels/v1/API population-levels.js");
-const energy_consumptions_API = require("./src/back/energy-consumptions.js");
+const energy_consumptions_API = require("./src/back/energy-consumptions/v2/energy-consumptions.js");
+const energy_consumptions_API_v1 = require("./src/back/energy-consumptions/v1/energy-consumptions.js");
 const internet_population_API = require("./src/back/internet-population.js");
 const Datastore = require('nedb');
 
@@ -17,6 +18,7 @@ db_population_levels = new Datastore();
 db_population_levels_v1 = new Datastore();
 db_internet_population = new Datastore();
 db_energy_consumptions = new Datastore();
+db_energy_consumptions_v1 = new Datastore();
 
 app.use(bodyParser.json());
 
@@ -26,6 +28,7 @@ app.use(bodyParser.json());
 population_levels_API.register(app,db_population_levels);
 population_levels_API_v1.register(app,db_population_levels_v1);
 energy_consumptions_API.register(app, db_energy_consumptions);
+energy_consumptions_API_v1.register(app, db_energy_consumptions_v1);
 internet_population_API.register(app, db_internet_population);
 
 app.use("/", express.static('public'));
